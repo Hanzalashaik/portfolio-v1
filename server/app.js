e51a74dd3daa75d6425dba754d8f139e216894d7
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-08-15 18:37:27 +0530

    server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const config = require("config");
+import express from "express"
 
 const app = express();
 
+const port = 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
-}); 
-
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
